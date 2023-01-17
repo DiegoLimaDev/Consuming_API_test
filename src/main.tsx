31fd985fc7ca750dfca13ea8573c68596fed6bcc
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-17 15:32:40 -0300

    refactor: building

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={'Consuming_API_test' || ''}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
