caa8612c2a5ba1c8f66661f9f2daa5e71f3f97dd
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-17 15:46:08 -0300

    refactor: building

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={'Consuming_API_test' || ''}>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://diegolimadev.github.io/Consuming_API_test/',
   plugins: [react()],
 });
