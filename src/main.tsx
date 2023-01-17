67ef5e8c5cbd41edf34c4726b8c14f78a1c0de97
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-17 15:25:24 -0300

    refactor: building

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename='/diegolimadev'>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
