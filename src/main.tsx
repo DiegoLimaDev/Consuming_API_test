0237687f28b538ec0ef9cf202053d2a18ef6ab13
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-17 19:25:11 -0300

    feat: routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { GlobalContext } from './utils/GlobalContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalContext>
+      <Header />
+      <BrowserRouter basename={window.location.pathname || ''}>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext>
   </React.StrictMode>,
 );
