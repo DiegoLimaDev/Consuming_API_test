9ad2cb75d59aa4ce5606c760c2d524234f7fece5
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-18 20:13:26 -0300

    refactor: dynamic search

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,29 +1,25 @@
 import { Icon } from '@iconify/react';
 import { IconButton, InputAdornment } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { useSearchValue } from '../../utils/GlobalContext';
 import { theme } from '../../utils/theme';
 import { Container, CustomInput } from './SearchInputStyle';
 
 export const SearchInput = () => {
   const { value, setValue } = useSearchValue();
-  const [string, setString] = useState('');
+  // const [string, setString] = useState('');
 
   return (
     <Container>
       <CustomInput
-        value={string}
-        onChange={(e) => setString(e.target.value)}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         placeholder="Pesquisar"
         type="text"
-        onKeyDown={(e) => (e.key === 'Enter' ? setValue(string) : null)}
+        // onKeyDown={(e) => (e.key === 'Enter' ? setValue(string) : null)}
         endAdornment={
           <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              edge="end"
-              onClick={() => setValue(string)}
-            >
+            <IconButton aria-label="toggle password visibility" edge="end">
               <Icon
                 icon="ion:search-outline"
                 width={25}
