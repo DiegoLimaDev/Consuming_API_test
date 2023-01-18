99563348063b2fa3d3aa1c2c9a240bff3a56911a
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-18 17:08:53 -0300

    feat: search, drawer and number context created

--- src/utils/GlobalContext.tsx
+++ src/utils/GlobalContext.tsx
@@ -1,17 +1,38 @@
 import React, { createContext, useState } from 'react';
 import P from 'prop-types';
 
-type isDarkModeType = {
+type MyContextTypes = {
   isDarkMode: string;
   setIsDarkMode: React.Dispatch<React.SetStateAction<string>>;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  isDrawerVisible: boolean;
+  setIsDrawerVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  numberId: any;
+  setNumberId: React.Dispatch<React.SetStateAction<any>>;
 };
 
-export const MyContext = createContext<isDarkModeType>({} as isDarkModeType);
+export const MyContext = createContext<MyContextTypes>({} as MyContextTypes);
 
 export const GlobalContext = ({ children }: { children: any }) => {
   const [isDarkMode, setIsDarkMode] = useState('light');
+  const [value, setValue] = useState('');
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+  const [numberId, setNumberId] = useState();
+
   return (
-    <MyContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+    <MyContext.Provider
+      value={{
+        isDarkMode,
+        setIsDarkMode,
+        value,
+        setValue,
+        isDrawerVisible,
+        setIsDrawerVisible,
+        numberId,
+        setNumberId,
+      }}
+    >
       {children}
     </MyContext.Provider>
   );
@@ -22,3 +43,9 @@ GlobalContext.propTypes = {
 };
 
 export const useIsDarkMode = () => React.useContext(MyContext);
+
+export const useSearchValue = () => React.useContext(MyContext);
+
+export const useIsDrawerVisible = () => React.useContext(MyContext);
+
+export const useNumberId = () => React.useContext(MyContext);
