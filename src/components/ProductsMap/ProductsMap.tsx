9deeb89e4d10321f97bdcbbde27b3a18119036a0
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-18 17:07:28 -0300

    feat: finalization of the component

--- src/components/ProductsMap/ProductsMap.tsx
+++ src/components/ProductsMap/ProductsMap.tsx
@@ -1,15 +1,77 @@
 import React from 'react';
 import { dataShape } from '../../services/interface';
 import P from 'prop-types';
+import {
+  useIsDarkMode,
+  useIsDrawerVisible,
+  useNumberId,
+  useSearchValue,
+} from '../../utils/GlobalContext';
+import { Text } from '../Text/Text';
+import {
+  ButtonContainer,
+  CustomImage,
+  Item,
+  MyGrid,
+  NotFoundContainer,
+} from './ProductsMapsStyle';
+import { Grid, Stack } from '@mui/material';
+import { CustomDrawer } from '../Drawer/Drawer';
+import { Button } from '../Button/Button';
 
 export const ProductsMap = ({ data }: { data: dataShape[] }) => {
+  const { value, setValue } = useSearchValue();
+  const { isDarkMode, setIsDarkMode } = useIsDarkMode();
+  const { isDrawerVisible, setIsDrawerVisible } = useIsDrawerVisible();
+  const { numberId, setNumberId } = useNumberId();
+
+  const filteredData = data.filter((e) =>
+    e.title.toLowerCase().includes(value.toLowerCase()),
+  );
+
   return (
     <>
-      {data.map((e) => (
-        <div key={e.id}>
-          <p>{e.price}</p>
-        </div>
-      ))}
+      {filteredData.length === 0 ? (
+        <NotFoundContainer>
+          <Text size="title" darkmode={isDarkMode}>
+            Dados não encontrados
+          </Text>
+        </NotFoundContainer>
+      ) : (
+        <Grid container rowSpacing={1} columnSpacing={5}>
+          {filteredData.map((e) => (
+            <MyGrid item xs={4} md={2} key={e.id} darkmode={isDarkMode}>
+              <Item>
+                <CustomImage src={e.image} height="200" width="200" />
+                <Text
+                  size="medium2"
+                  darkmode="light"
+                  align="center"
+                  margin="2rem 0 0 0"
+                >
+                  {`${e.title.substring(0, 30)}...  `}
+                </Text>
+                <Text size="medium2" darkmode="light" align="center">
+                  {`R$${e.price}`}
+                </Text>
+                <Stack direction="row">
+                  <ButtonContainer>
+                    <Button
+                      onClick={() => {
+                        setIsDrawerVisible(!isDrawerVisible);
+                        setNumberId(e.id);
+                      }}
+                    >
+                      Ver informações
+                    </Button>
+                  </ButtonContainer>
+                </Stack>
+              </Item>
+            </MyGrid>
+          ))}
+          <CustomDrawer visible={isDrawerVisible} data={data} />
+        </Grid>
+      )}
     </>
   );
 };
--- src/components/ProductsMap/ProductsMapsStyle.tsx
+++ src/components/ProductsMap/ProductsMapsStyle.tsx
@@ -1,4 +1,44 @@
 import styled from '@emotion/styled';
+import { Grid, Paper, Rating } from '@mui/material';
 import { Box, css } from '@mui/system';
+import { theme } from '../../utils/theme';
 
-export const Container = styled(Box)(() => css``);
+export const NotFoundContainer = styled(Box)(
+  () => css`
+    display: block;
+    margin: 10rem auto;
+    width: fit-content;
+    min-height: 30rem;
+  `,
+);
+
+export const Item = styled(Paper)(
+  () => css`
+    padding: 2rem;
+    text-align: 'center';
+    color: ${theme.colors.black};
+    height: 35rem;
+  `,
+);
+
+export const MyGrid = styled(Grid)(
+  ({ darkmode }: { darkmode: string }) => css`
+    margin: 5rem 2rem 0 4rem;
+    min-height: 50rem;
+    background-color: ${darkmode === 'dark'
+      ? theme.colors.black
+      : theme.colors.iceWhite};
+    width: fit-content;
+  `,
+);
+
+export const CustomImage = styled.img`
+  display: block;
+  margin: 0 auto;
+`;
+
+export const ButtonContainer = styled(Box)(
+  () => css`
+    margin-top: 2rem;
+  `,
+);
