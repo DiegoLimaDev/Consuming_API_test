5fbc99a1b972c907ae90487b02e85acf08a1f896
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-18 17:08:01 -0300

    feat: new props created

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -6,20 +6,26 @@ export const Text = ({
   children,
   size,
   darkmode,
+  align,
+  margin,
 }: {
-  children: any;
+  children?: any;
   size: string;
-  darkmode: string;
+  darkmode?: string;
+  align?: string;
+  margin?: string;
 }) => {
   return (
-    <Container size={size} darkmode={darkmode}>
+    <Container size={size} darkmode={darkmode} align={align} margin={margin}>
       <p>{children}</p>
     </Container>
   );
 };
 
 Text.propTypes = {
-  children: P.any.isRequired,
+  children: P.any,
   size: P.string.isRequired,
   darkmode: P.string,
+  align: P.string,
+  margin: P.string,
 };
--- src/components/Text/TextStyles.tsx
+++ src/components/Text/TextStyles.tsx
@@ -3,15 +3,29 @@ import { Box, css } from '@mui/material';
 import { theme } from '../../utils/theme';
 
 export const Container = styled(Box)(
-  ({ size, darkmode }: { size: string; darkmode: string }) => css`
+  ({
+    size,
+    darkmode,
+    align,
+    margin,
+  }: {
+    size: string;
+    darkmode?: string;
+    align?: string;
+    margin?: string;
+  }) => css`
     font-family: 'Nunito', sans-serif;
     font-size: ${size === 'small'
       ? theme.sizes.small
       : size === 'medium'
       ? theme.sizes.medium
+      : size === 'medium2'
+      ? theme.sizes.medium2
       : size === 'big'
       ? theme.sizes.big
       : theme.sizes.title};
     color: ${darkmode === 'dark' ? theme.colors.iceWhite : theme.colors.black};
+    text-align: ${align};
+    margin: ${margin};
   `,
 );
