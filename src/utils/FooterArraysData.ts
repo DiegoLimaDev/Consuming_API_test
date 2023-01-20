0ef377d1afe797bd6ef97ef15546777829e60a39
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-20 11:07:17 -0300

    refactor: utils folder with some global needed itens

---
+++ src/utils/FooterArraysData.ts
@@ -0,0 +1,13 @@
+export const icons = [
+  'mdi:github',
+  'mdi:email-outline',
+  'ant-design:linkedin-outlined',
+  'ic:outline-whatsapp',
+];
+
+export const links = [
+  'https://github.com/diegolimadev',
+  'mailto:xxdiegolsxx@gmail.com',
+  'https://www.linkedin.com/in/diegolimadev/',
+  'https://api.whatsapp.com/send?phone=5581997086223&text=Tentei%20facilitar%20o%20m%C3%A1ximo%20pra%20voc%C3%AAs%20me%20darem%20a%20not%C3%ADcia%20da%20aprova%C3%A7%C3%A3o.%20Por%20favor,%20n%C3%A3o%20me%20fa%C3%A7am%20uma%20desfeita!%20hahaha',
+];
---
+++ src/utils/selectArraysData.ts
@@ -0,0 +1,41 @@
+export const selectCategory = [
+  {
+    value: 'All',
+    label: 'Todas',
+  },
+  {
+    value: `men's clothing`,
+    label: 'Moda masculina',
+  },
+  {
+    value: `women's clothing`,
+    label: 'Moda feminina',
+  },
+  {
+    value: 'electronics',
+    label: 'Eletrônicos',
+  },
+  {
+    value: 'Jewelery',
+    label: 'Joias',
+  },
+];
+
+export const selectOrder = [
+  {
+    value: 'initial',
+    label: 'Original',
+  },
+  {
+    value: `highest`,
+    label: 'Maior preço',
+  },
+  {
+    value: `lowest`,
+    label: 'Menor preço',
+  },
+  {
+    value: 'rating',
+    label: 'Avaliações',
+  },
+];
